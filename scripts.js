d525716d5daadf872b9c129bbf49dbeb5ec9cac1
Brianna DelValle <briannadelvalle@gmail.com>
2019-08-29 14:14:47 -0600

    Query Select inputs and buttons

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var ideas = []
+
+// querySelectors
+
+var titleInput = document.querySelector('input[name="title"]');
+var bodyInput = document.querySelector('input[name="body"]');
+var qualityInput = document.querySelector('input[name="quality"]');
+
+var showStarredButton = document.querySelector('.starred-filter button')
+var plausibleButton = document.querySelector('.quality-filter button:nth-of-type(2)')
+var geniusButton = document.querySelector('.quality-filter button:nth-of-type(3)')
+var addQualityButton = document.querySelector('.quality-form button');
+var saveNewIdeaButton = document.querySelector('.idea-form button:nth-of-type(1)');
+var searchIdeasButton = document.querySelector('.idea-form div button')
+
+
