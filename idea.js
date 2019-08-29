7b4e27a94831e484025074d04eba8eab503e8aab
Brianna DelValle <briannadelvalle@gmail.com>
2019-08-29 16:49:49 -0600

    Style everything

--- idea.js
+++ idea.js
@@ -19,17 +19,18 @@ class Idea {
       `<article data-id=${this.id}>
         <div class="card-buttons">
           <button class="delete">x</button>
-          <button class="star ${className}">*</button>
-        </div>
-        <div class="idea-content">
           <h5 contentEditable="true">${this.title}</h5>
-          <p contentEditable="true">${this.body}</p>
+          <button class="star ${className}">*</button>
         </div>
         <div class="card-quality">
           <button>up</button>
           <span>${this.qualities[this.quality]}</span>
           <button>down</button>
         </div>
+        <div class="idea-content">
+          <p contentEditable="true">${this.body}</p>
+        </div>
+        
       </article>`
     )
   }
